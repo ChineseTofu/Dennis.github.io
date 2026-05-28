4318808ea56f1fc466bb02d7670292e56f2c35e5
AsianScammer <dennis29012006@gmail.com>
2026-05-28 21:31:24 +0200

    take 6

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'motion/react';
 import { X, Github, ExternalLink } from 'lucide-react';
+const image = import.meta.env.BASE_URL + 'RampPhoenix.png';
 
 
 const projects = [
