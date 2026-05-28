848a8f0fc34b21f5c0f786fb1343aca7147e00d6
AsianScammer <dennis29012006@gmail.com>
2026-05-28 19:09:55 +0200

    a

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-    base: '/DennisPortfolio.github.io/', 
+    base: '/Dennis.github.io/', 
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
